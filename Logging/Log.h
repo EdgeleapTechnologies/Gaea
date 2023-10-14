c9d5b35336041628c8f28615ee8380a6cbd8ef93
TiagoJoseMagalhaes <tiagojsmagalhaes@gmail.com>
2023-10-14 01:12:47 +0100

    fixes related to log function

--- Logging/Log.h
+++ Logging/Log.h
@@ -5,8 +5,6 @@
 namespace Edgeleap
 {
 
-#define ERROR_LOG(X, ...) printf(X, __VA_ARGS__);
-#define WARNING_LOG(X, ...) printf(X, __VA_ARGS__);
-#define SUCCESS_LOG(X, ...) printf(X, ##__VA_ARGS__);
+#define SUCCESS_LOG(s, ...) printf(s __VA_OPT__(, ) __VA_ARGS__)
 
 }
