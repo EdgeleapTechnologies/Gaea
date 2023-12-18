3f66cc42797d4f58486395b905a8cfc9a628ee41
Tiago José Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2023-12-18 00:10:26 +0000

    Function construction and callability tests

--- tests/Core/Function_UnitTests.cpp
+++ tests/Core/Function_UnitTests.cpp
@@ -1,13 +1,38 @@
 #include <Core/Function.h>
+#include <catch2/catch_test_macros.hpp>
 
-namespace Edgealeap
+namespace Edgeleap
 {
 
 namespace Test
 {
 
-TEST_CASE("")
+int test_function_1()
 {
+    return 55;
+}
+
+TEST_CASE("Function can contain any callable", "Construction & operator() testing")
+{
+    SECTION("empty constructor, call operator should return default return type value")
+    {
+        Edgeleap::Function<int(void)> empty_constructor_function;
+        REQUIRE(empty_constructor_function() == 0);
+    }
+
+    SECTION("Static Pre-defined function")
+    {
+        Edgeleap::Function<int(void)> static_function(test_function_1);
+        REQUIRE(static_function() == 55);
+    }
+
+    SECTION("Lambda without capture group")
+    {
+    }
+
+    SECTION("Lambda with capture group")
+    {
+    }
 }
 
 }
