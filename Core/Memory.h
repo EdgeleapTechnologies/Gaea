c1b2cc15b998c9d51dde310bf71db7097dd62e1c
Tiago José Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2023-12-25 23:37:49 +0000

    Adds allocator interface and global allocator interface impl

--- Core/Memory.h
+++ Core/Memory.h
@@ -10,4 +10,39 @@ void* GlobalAllocate(size_t size);
 void GlobalFree(void* ptr);
 void MemCopy(void* destination, void* source, size_t size);
 
+// Allocator interface that can be used as generic allocator input
+// for other methods
+class Allocator
+{
+protected:
+    Allocator() = default;
+
+public:
+    virtual ~Allocator() = default;
+    Allocator(const Allocator& other) = delete;
+    Allocator(Allocator&& other) = delete;
+    Allocator& operator=(const Allocator& other) = delete;
+    Allocator& operator=(Allocator&& other) = delete;
+
+    virtual void* Allocate(size_t length) = 0;
+    virtual void Free(void* ptr) = 0;
+};
+
+class SystemAllocator: public Allocator
+{
+public:
+    SystemAllocator() = default;
+
+    inline void* Allocate(size_t length) override
+    {
+        return GlobalAllocate(length);
+    }
+
+    inline void Free(void* ptr) override
+    {
+        GlobalFree(ptr);
+    }
+};
+
+
 }
