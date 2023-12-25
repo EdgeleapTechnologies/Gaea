97a38f6baf1448ba680bba0277df60b55377ca6e
Tiago José Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2023-12-25 23:37:53 +0000

    Adds allocator interface and global allocator interface impl

---
+++ tests/Core/DataStructures/StringTests.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch_test_macros.hpp>
+#include <Core/DataStructures/String.h>
+
+namespace Edgeleap
+{
+
+namespace Test
+{
+
+TEST_CASE("String Implementation", "Constructors")
+{
+    String str = "test";
+    REQUIRE(false == true);
+}
+
+}
+
+}
