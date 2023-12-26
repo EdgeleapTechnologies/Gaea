b6d8a4f4048a2da53a70dea22384c09385497f6d
TiagoJoseMagalhaes <tiagojsmagalhaes@gmail.com>
2023-12-26 03:42:25 +0000

    fixes style issues with string.cpp

--- Core/DataStructures/String.cpp
+++ Core/DataStructures/String.cpp
@@ -6,90 +6,90 @@
 namespace Edgeleap
 {
 
-    String::String(const char* string)
+String::String(const char* string)
+{
+    this->m_string_length = String::ComputeStringLength(string);
+    this->m_string = (char*)GlobalAllocate(this->m_string_length);
+    MemCopy((void*)this->m_string.ptr, (void*)string, this->m_string_length);
+}
+
+String::String(const char* string, size_t length)
+{
+    this->m_string_length = length;
+    this->m_string = (char*)GlobalAllocate(this->m_string_length);
+    MemCopy((void*)this->m_string.ptr, (void*)string, this->m_string_length);
+}
+
+String::String(const String& other)
+{
+    if(&other == this) return;
+
+    this->m_string_length = other.m_string_length;
+    MemCopy((void*)this->m_string.ptr, (void*)other.m_string.ptr, this->m_string_length);
+}
+
+String& String::operator=(const String& other)
+{
+    if(&other == this) return *this;
+
+    this->m_string_length = other.m_string_length;
+    MemCopy((void*)this->m_string.ptr, (void*)other.m_string.ptr, this->m_string_length);
+
+    return *this;
+}
+
+String::String(String&& other)
+{
+    if(&other == this) return;
+
+    this->m_string_length = other.m_string_length;
+    other.m_string_length = 0;
+
+    this->m_string = move(other.m_string);
+}
+
+String& String::operator=(String&& other)
+{
+    if(&other == this) return *this;
+
+    this->m_string_length = other.m_string_length;
+    other.m_string_length = 0;
+
+    this->m_string = move(other.m_string);
+
+    return *this;
+}
+
+char& String::operator[](const size_t index)
+{
+    return this->m_string.ptr[index];
+}
+
+char String::at(const size_t index) const
+{
+    return this->m_string.ptr[index];
+}
+
+size_t String::legth() const
+{
+    return this->m_string_length;
+}
+
+const char* String::data() const
+{
+    return this->m_string.ptr;
+}
+
+//TODO(Tiago):very naive strlen implementation, in the future improve it
+//using SIMD
+size_t String::ComputeStringLength(const char* string)
+{
+    size_t counter = 0;
+    while(string[counter] != 0)
     {
-        this->m_string_length = String::ComputeStringLength(string);
-        this->m_string = (char*)GlobalAllocate(this->m_string_length);
-        MemCopy((void*)this->m_string.ptr, (void*)string, this->m_string_length);
+        counter++;
     }
-
-    String::String(const char* string, size_t length)
-    {
-        this->m_string_length = length;
-        this->m_string = (char*)GlobalAllocate(this->m_string_length);
-        MemCopy((void*)this->m_string.ptr, (void*)string, this->m_string_length);
-    }
-
-    String::String(const String& other)
-    {
-        if(&other == this) return;
-
-        this->m_string_length = other.m_string_length;
-        MemCopy((void*)this->m_string.ptr, (void*)other.m_string.ptr, this->m_string_length);
-    }
-
-    String& String::operator=(const String& other)
-    {
-        if(&other == this) return *this;
-
-        this->m_string_length = other.m_string_length;
-        MemCopy((void*)this->m_string.ptr, (void*)other.m_string.ptr, this->m_string_length);
-
-        return *this;
-    }
-
-    String::String(String&& other)
-    {
-        if(&other == this) return;
-
-        this->m_string_length = other.m_string_length;
-        other.m_string_length = 0;
-
-        this->m_string = move(other.m_string);
-    }
-
-    String& String::operator=(String&& other)
-    {
-        if(&other == this) return *this;
-
-        this->m_string_length = other.m_string_length;
-        other.m_string_length = 0;
-
-        this->m_string = move(other.m_string);
-
-        return *this;
-    }
-
-    char& String::operator[](const size_t index)
-    {
-        return this->m_string.ptr[index];
-    }
-
-    char String::at(const size_t index) const
-    {
-        return this->m_string.ptr[index];
-    }
-
-    size_t String::legth() const
-    {
-        return this->m_string_length;
-    }
-
-    const char* String::data() const
-    {
-        return this->m_string.ptr;
-    }
-
-    //TODO(Tiago):very naive strlen implementation, in the future improve it
-    //using SIMD
-    size_t String::ComputeStringLength(const char* string)
-    {
-        size_t counter = 0;
-        while(string[counter] != 0)
-        {
-            counter++;
-        }
-        return counter;
-    }
+    return counter;
+}
 
 }
